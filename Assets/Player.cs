541f84b6904da395b1b1bad3b54ff3a4f7225272
Nerond32 <nerond32@gmail.com>
2017-10-29 14:21:11 +0100

    skroling background

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && this.transform.position.x > -6)
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - 3.5f;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && this.transform.position.x < 6)
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + 3.5f;
+            this.transform.position = position;
+        }
+    }
+}
---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    static Vector3 initPosition;
+    void Start()
+    {
+        initPosition = this.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 position = this.transform.position;
+        if (position.y < -8.9f)
+        {
+            position = initPosition;
+        }
+        else
+        {
+            position.y = position.y - 0.1f;
+        }
+        this.transform.position = position;
+    }
+}
